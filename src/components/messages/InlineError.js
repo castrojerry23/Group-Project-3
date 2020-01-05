4ac717e73b12515059429c968fb9d9b00a4fb1e3
SantiagoTZ8899 <santiago.tz89@gmail.com>
2020-01-05 17:58:16 -0600

    added prop-types package

--- src/components/messages/InlineError.js
+++ src/components/messages/InlineError.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const InlineError = ({ text }) => (
-    <div>
+    <span style={{color: "#af5855"}}>
+        {text}
 
-
-    </div>
+    </span>
 );
+
+InlineError.propTypes = {
+    text: PropTypes.string.isRequired
+};
 
 export default InlineError;
