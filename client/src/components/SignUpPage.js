d7b5e275686ff078dcaa06ddfebaf7e091985589
Jerry Castro <castrojerry23@gmail.com>
2020-01-18 11:29:01 -0600

    removed card from signup page

--- client/src/components/SignUpPage.js
+++ client/src/components/SignUpPage.js
@@ -5,7 +5,7 @@ import './Components.css';
 import SignUpFormContainer from '../containers/SignUpFormContainer';
 
 export default () => (
-  <div className="card container">
+  <div className="container">
     <div className="row">
       <div className="col-md-4 offset-md-4">
         <h1 className="signupform">Sign up</h1>
